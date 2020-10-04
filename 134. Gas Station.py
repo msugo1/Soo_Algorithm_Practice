9b5e7776799b56cbcc909deb7ad3de6c72ea2370
Soo <gomsu1045@gmail.com>
2020-10-04 12:10:05 +0900

    134. Gas Station.py
    4 OCT. 2020: Algorithm Practice with Python Algorithm Interview

--- 134. Gas Station.py
+++ 134. Gas Station.py
@@ -87,3 +87,68 @@ class Solution:
 # I feel like I can reach the answer but !!!! can't... so frustrated
 # so I'll leave this question for tomorrow (or maybe i'll give it another try later today)
 
+# 04 OCT 2020
+# what I couldn't come up with: the range of indices, start ~ len(gas) + start means it will circle one loop
+# also using boolean
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        fuel = 0
+
+        starting_indices = []
+
+        for i in range(len(gas)):
+            if gas[i] - cost[i] >= 0:
+                starting_indices.append(i)
+
+        if len(starting_indices) == 0:
+            return -1
+
+        for start in starting_indices:
+            for i in range(start, len(gas) + start):
+                location = i % len(gas)
+
+                can_travel = True
+                if fuel + gas[location] < cost[location]:
+                    can_travel = False
+                    break
+                else:
+                    fuel += (gas[location] - cost[location])
+
+            if can_travel:
+                return start
+
+        return -1
+
+# result - wrong answer, when gas = [5, 1, 2, 3, 4] / cost = [4, 4, 1, 5, 1]
+# the output should be 4 but it returns 2
+# spotted the reason - I did not reset fuel after a loop is done
+
+# basically what I haven't been able to solve this problem since yesterday was because I forgot to reset fuel as 0 (regardless of the runtime)
+# runtime is dreadful though... just flabbergasted for the fact that it did not exceed the time limit
+
+# result: runtime - 8192m/s, 4532m/s (each 11% and 5%)
+
+# well because it was O(n^2). There must be a way to make it a linear time complexity O(n)
+
+# a solution from Python Algorithm Interview
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        # can come back to the starting point only if the total amount of gas outstrips that of cost
+        if sum(gas) < sum(cost):
+            return -1
+
+        # there must be ONE POINT of possibility, so if cost outpaces gas + fuel in the location,
+        # exclude the location and move the starting point to the next
+        # or keep adding fuel till the end
+        start, fuel = 0, 0
+        for i in range(len(gas)):
+            if gas[i] + fuel < cost[i]:
+                start = i + 1
+                fuel = 0
+            else:
+                fuel += (gas[i] - cost[i])
+
+        # here, now what start points at should be the possible point
+        return start
